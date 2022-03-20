b04bbdc0fcf9ebc4fec313a9a6f19b5134a61ac3
christopher lai <christophercylai@outlook.com>
2022-03-20 03:06:13 -0400

    fixed zeroed with abs

--- newton_rapson/newton_rapson.cpp
+++ newton_rapson/newton_rapson.cpp
@@ -8,7 +8,7 @@
 class Term
 {
     public:
-        Term(int d, int m) {
+        explicit Term(int d, int m) {
             degree = d;
             multiplier = m;
         }
@@ -30,7 +30,7 @@ class Term
 class Function
 {
     public:
-        Function() {
+        explicit Function() {
             int degree;
             std::cout << "Enter the highest degree of f(x)" << std::endl;
             std::cin >> degree;
@@ -48,14 +48,18 @@ class Function
             differentiate();
         }
 
-        float newton_raphson(float xn) {
-            return (xn - (calcFunc(function, xn)/calcFunc(diff_func, xn)));
+        double newton_raphson(const double& xn) {
+            double ret = xn - (calcFunc(function, xn)/calcFunc(diff_func, xn));
+            if (std::isnan(ret)) {
+                throw std::runtime_error("The supplied starting point has a slope of 0");
+            }
+            return ret;
         }
 
-        bool zeroed(const float& xn) {
+        bool zeroed(const double& xn) {
             // check if f(xn) results in 0
-            const float y = calcFunc(function, xn);
-            if (y < 0.0000001) {
+            const double y = calcFunc(function, xn);
+            if (std::abs(y) < 0.000000000001) {
                 return true;
             }
             return false;
@@ -89,8 +93,8 @@ class Function
             }
         }
 
-        static float calcFunc(std::vector<Term>& func, float xn) {
-            float xn_1 = 0;
+        static double calcFunc(std::vector<Term>& func, double xn) {
+            double xn_1 = 0;
             for (auto& f : func) {
                 xn_1 += f.multiplier * std::pow(xn, f.degree);
             }
@@ -106,7 +110,7 @@ int main() {
     // get the function from user input
     Function func = Function();
 
-    float xn;
+    double xn;
     std::cout << "Enter a number close to the root" << std::endl;
     std::cin >> xn;
     std::cout << std::endl;
@@ -114,13 +118,14 @@ int main() {
 
     // applying newton-raphson method
     bool zeroed = false;
-    int count = 10;
-    while (! zeroed) {
+
+    for (int count=0; count <= 100; count++) {
+        if (count == 100) {
+            throw std::runtime_error("Root does not exist");
+        }
         xn = func.newton_raphson(xn);
-        zeroed = func.zeroed(xn);
-        count = count - 1;
-        if (count < 0) {
-            throw std::runtime_error("No solution - there is no root near " + x_init);
+        if (func.zeroed(xn)) {
+            break;
         }
     }
 
